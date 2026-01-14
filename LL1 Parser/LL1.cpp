57598301747e6d76fce4a59f6202d1a92e819187
Aditya <aditya07.11.04.12@gmail.com>
2026-01-14 17:56:53 +0530

    Added follow logic for the given LL1 parser

--- LL1 Parser/LL1.cpp
+++ LL1 Parser/LL1.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 unordered_set<string> terminals={"+","(",")","id","*","null"};
 unordered_set<string> non_terminals={"E","T","F"};
+string start="E";
 
 vector<string> splitter(string pattern,string s)
 {
@@ -115,6 +116,150 @@ unordered_map<string,unordered_set<string>> first_sets(unordered_map<string,vect
     return first_map;
 }
 
+
+unordered_set<string> follow(unordered_set<string> terminals,unordered_set<string> non_terminals,string character,unordered_map<string,vector<string>> mp, unordered_map<string,unordered_set<string>>& memo, unordered_map<string,unordered_set<string>>& first_map)
+{
+    
+    if(memo.find(character) != memo.end())
+    {
+        return memo[character];
+    }
+    
+    unordered_set<string> follow_set;
+    
+    
+    if(character == start)
+    {
+        follow_set.insert("$");
+    }
+    
+    
+    for(auto& prod_entry : mp)
+    {
+        string lhs = prod_entry.first;
+        vector<string> productions = prod_entry.second;
+        
+        for(auto& production : productions)
+        {
+            
+            size_t pos = 0;
+            while(pos < production.length())
+            {
+                
+                bool found = false;
+                if(pos < production.length() && production.substr(pos, character.length()) == character)
+                {
+                    
+                    if(pos + character.length() == production.length() || 
+                       non_terminals.find(string(1, production[pos + character.length()])) != non_terminals.end() ||
+                       terminals.find(string(1, production[pos + character.length()])) != terminals.end())
+                    {
+                        found = true;
+                    }
+                }
+                
+                if(found)
+                {
+                    
+                    string beta = production.substr(pos + character.length());
+                    
+                    if(beta.empty())
+                    {
+                        
+                        if(lhs != character) 
+                        {
+                            unordered_set<string> follow_lhs = follow(terminals, non_terminals, lhs, mp, memo, first_map);
+                            follow_set.insert(follow_lhs.begin(), follow_lhs.end());
+                        }
+                    }
+                    else
+                    {
+                       
+                        string beta_first_symbol = "";
+                        
+                        
+                        bool beta_is_terminal = false;
+                        for(auto& term : terminals)
+                        {
+                            if(beta.find(term) == 0)
+                            {
+                                beta_first_symbol = term;
+                                beta_is_terminal = true;
+                                break;
+                            }
+                        }
+                        
+                        
+                        if(!beta_is_terminal)
+                        {
+                            beta_first_symbol = string(1, beta[0]);
+                        }
+                        
+                        
+                        if(first_map.find(beta_first_symbol) != first_map.end())
+                        {
+                            unordered_set<string> first_beta = first_map[beta_first_symbol];
+                            bool has_null = false;
+                            
+                            for(auto& sym : first_beta)
+                            {
+                                if(sym != "null")
+                                {
+                                    follow_set.insert(sym);
+                                }
+                                else
+                                {
+                                    has_null = true;
+                                }
+                            }
+                            
+                            
+                            if(has_null && lhs != character)
+                            {
+                                unordered_set<string> follow_lhs = follow(terminals, non_terminals, lhs, mp, memo, first_map);
+                                follow_set.insert(follow_lhs.begin(), follow_lhs.end());
+                            }
+                        }
+                        else if(beta_is_terminal)
+                        {
+                            follow_set.insert(beta_first_symbol);
+                        }
+                    }
+                    
+                    pos += character.length();
+                }
+                else
+                {
+                    pos++;
+                }
+            }
+        }
+    }
+    
+    memo[character] = follow_set;
+    return follow_set;
+}
+
+
+
+unordered_map<string,unordered_set<string>> follow_sets(unordered_map<string,vector<string>> &mp)
+{
+    
+    unordered_map<string,unordered_set<string>> first_map = first_sets(mp);
+    
+    unordered_map<string,unordered_set<string>> follow_map;
+    unordered_map<string,unordered_set<string>> memo;
+    
+    for(auto it:mp)
+    {
+        string character=it.first;
+        follow_map[character]=follow(terminals,non_terminals,character,mp,memo,first_map);
+    }
+    return follow_map;
+}
+
+
+
 int main()
 {
     string productions;
@@ -164,6 +309,19 @@ int main()
     for(auto &it:first_sets_productions)
     {
         cout<<"first of "<<it.first<<" = ";
+        for(auto it2:it.second)
+        {
+            cout<<it2<<" ";
+        }
+        cout<<endl;
+    }
+    
+    unordered_map<string,unordered_set<string>> follow_sets_productions=follow_sets(production_map);
+    cout<<endl;
+
+    for(auto &it:follow_sets_productions)
+    {
+        cout<<"follow of "<<it.first<<" = ";
         for(auto it2:it.second)
         {
             cout<<it2<<" ";
